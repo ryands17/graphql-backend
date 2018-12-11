f1fe12cbf085e438d784016f21f741526fd15504
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2018-12-12 00:11:25 +0530

    barebones graphql server

---
+++ src/index.js
@@ -0,0 +1,66 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+]
+
+let idCount = links.length
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (_, { id }) => {
+      try {
+        let link = links.find(l => l.id === id)
+        return link || null
+      } catch (e) {
+        return null
+      }
+    },
+  },
+  Mutation: {
+    post: (_, { url, description }) => {
+      let link = {
+        id: `link-${idCount++}`,
+        description,
+        url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (_, { id, ...rest }) => {
+      let index = links.findIndex(l => l.id === id)
+      if (index === -1) return null
+      console.log('l', links[index])
+      let newLink = {
+        ...links[index],
+        ...rest,
+      }
+      links[index] = newLink
+      return newLink
+    },
+    deleteLink: (_, { id }) => {
+      let index = links.findIndex(l => l.id === id)
+      if (index === -1) return null
+      return links.splice(index, 1)[0]
+    },
+  },
+  /* not required as gql resolves this automatically */
+  // Link: {
+  //   id: root => root.id,
+  //   description: root => root.description,
+  //   url: root => root.url,
+  // },
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+})
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
